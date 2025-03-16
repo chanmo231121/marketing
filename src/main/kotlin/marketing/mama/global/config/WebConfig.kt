9a6284a31e146578b27f7dec91175a89e756e016
chanmoKIM <abczza@naver.com>
2025-03-16 22:21:58 +0900

    CORS 문제

--- src/main/kotlin/marketing/mama/global/config/WebConfig.kt
+++ src/main/kotlin/marketing/mama/global/config/WebConfig.kt
@@ -22,7 +22,8 @@ class WebConfig(
                 "http://localhost:8080",
                 "http://lohosttest.s3-website.ap-northeast-2.amazonaws.com",
                 "http://maglo4.s3-website.ap-northeast-2.amazonaws.com",
-                "http://43.203.93.162") // 프론트엔드 도메인 허용
+                "http://maglo6.s3-website.ap-northeast-2.amazonaws.com",
+                "http://43.203.93.162")
             .allowedMethods("*") // 모든 HTTP 메서드 허용
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 자격 증명 허용 (예: 쿠키, 인증)
