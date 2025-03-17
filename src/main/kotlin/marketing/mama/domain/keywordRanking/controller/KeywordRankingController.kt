be5159a8fe13351290c6edacdb5fbff301f82830
chanmoKIM <abczza@naver.com>
2025-03-17 23:32:33 +0900

    키워드 랭킹순위 프론트백엔드연결문제

--- src/main/kotlin/marketing/mama/domain/keywordRanking/controller/KeywordRankingController.kt
+++ src/main/kotlin/marketing/mama/domain/keywordRanking/controller/KeywordRankingController.kt
@@ -3,18 +3,22 @@ package marketing.mama.domain.keywordRanking.controller
 import io.swagger.v3.oas.annotations.Operation
 import marketing.mama.domain.keywordRanking.service.KeywordRankingService
 import org.springframework.web.bind.annotation.*
-
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 @RestController
 @RequestMapping("/api/naver-ads")
 class KeywordRankingController(private val keywordRankingService: KeywordRankingService) {
 
-
-
     @Operation(summary = "네이버 검색광고 입찰순위")
     @GetMapping("/search")
     fun searchNaverAds(@RequestParam keywords: String): List<Map<String, Any>> {
-        val keywordList = keywords.split(",").map { it.trim() }
+        // URL 디코딩
+        val decodedKeywords = URLDecoder.decode(keywords, StandardCharsets.UTF_8.toString())
+
+        // 디코딩된 키워드들을 쉼표로 나누어 리스트로 변환
+        val keywordList = decodedKeywords.split(",").map { it.trim() }
+
         return keywordRankingService.getNaverAdData(keywordList)
     }
 }
