f68e4a3445ee320a1d971b449071420994da56ed
chanmoKIM <abczza@naver.com>
2025-03-18 00:02:26 +0900

    키워드 랭킹순위 프론트백엔드연결문제

--- src/main/kotlin/marketing/mama/domain/keywordRanking/controller/KeywordRankingController.kt
+++ src/main/kotlin/marketing/mama/domain/keywordRanking/controller/KeywordRankingController.kt
@@ -13,11 +13,14 @@ class KeywordRankingController(private val keywordRankingService: KeywordRanking
     @Operation(summary = "네이버 검색광고 입찰순위")
     @GetMapping("/search")
     fun searchNaverAds(@RequestParam keywords: String): List<Map<String, Any>> {
-        // URL 디코딩
+        // 디코딩된 키워드 확인을 위한 로그 추가
         val decodedKeywords = URLDecoder.decode(keywords, StandardCharsets.UTF_8.toString())
+        println("디코딩된 키워드: $decodedKeywords") // 디버깅 로그
 
-        // 디코딩된 키워드들을 쉼표로 나누어 리스트로 변환
         val keywordList = decodedKeywords.split(",").map { it.trim() }
+
+        // 실제로 키워드 리스트 확인
+        println("최종 키워드 리스트: $keywordList")  // 디버깅 로그
 
         return keywordRankingService.getNaverAdData(keywordList)
     }
