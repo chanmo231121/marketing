e497d3220810b513dbc85f672b085547cdbbe807
chanmoKIM <abczza@naver.com>
2025-04-11 20:21:00 +0900

    UI다듬기 V1.0.17
    1. 리프레쉬토큰 테스트

--- src/main/kotlin/marketing/mama/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/marketing/mama/domain/user/service/UserServiceImpl.kt
@@ -87,8 +87,8 @@ class UserServiceImpl(
         // ✅ 보안 쿠키로 refresh token 저장
         val cookie = ResponseCookie.from("refresh_token", refreshToken)
             .httpOnly(true)
-            .secure(false) // HTTPS 환경에서는 true
-            .sameSite("None")
+            .secure(false) // HTTPS 되면 true로
+            .sameSite("Lax") // HTTP 환경에서 가장 안정적
             .path("/")
             .maxAge(7 * 24 * 60 * 60)
             .build()
