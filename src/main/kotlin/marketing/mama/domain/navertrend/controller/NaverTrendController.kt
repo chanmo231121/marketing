f1a40d5ed30bb2c325982b342cf709efb43872aa
chanmoKIM <abczza@naver.com>
2025-05-12 18:29:17 +0900

    V1.1.7 네이버트랜드 테스트

--- src/main/kotlin/marketing/mama/domain/navertrend/controller/NaverTrendController.kt
+++ src/main/kotlin/marketing/mama/domain/navertrend/controller/NaverTrendController.kt
@@ -13,6 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
+
 @RestController
 @RequestMapping("/api/naver")
 class NaverTrendController(
